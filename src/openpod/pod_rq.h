dd0a207e6fc8416cb036bfa91802169746f50388
dzavalishin <dz@dz.ru>
2015-06-25 19:23:22 +0300

    empty rq.h

---
+++ src/openpod/pod_rq.h
@@ -0,0 +1,19 @@
+#ifndef POD_RQ_H
+#define POD_RQ_H
+
+#include "pod_types.h"
+
+
+//********************************************************************
+//
+// OpenPOD
+//
+// Driver request structure and methods.
+//
+//********************************************************************
+
+
+
+
+
+#endif // POD_RQ_H
