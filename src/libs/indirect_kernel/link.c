5700565e19fac2a9633bb432f62f6f8a9f31ce41
dzavalishin <dz@dz.ru>
2015-06-27 23:27:48 +0300

    indirect kernel API sketch

---
+++ src/libs/indirect_kernel/link.c
@@ -0,0 +1,18 @@
+#include <pod_kernel_ap.h>
+
+// I we have no corresponding functions available at link time,
+// provide weak ones that call kernel through kernel_driver_api
+
+
+errno_t __attribute__((weak))
+pod_dev_link( struct pod_driver *drv, struct pod_device *dev )
+{
+	return drv->kernel_driver_api->pod_dev_link( dev, dev );
+}
+
+
+errno_t	__attribute__((weak))	
+pod_dev_unlink( struct pod_driver *drv, struct pod_device *dev )
+{
+	return drv->kernel_driver_api->pod_dev_unlink( dev, dev );
+}
