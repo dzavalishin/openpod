b996583cac77d2baf4b70369479986a22cc65790
dzavalishin <dz@dz.ru>
2015-06-28 10:14:02 +0300

    check for non-NULL func ptr

--- src/libs/indirect_kernel/link.c
+++ src/libs/indirect_kernel/link.c
@@ -1,5 +1,7 @@
 #include <pod_kernel_ap.h>
 
+#include <errno.h>
+
 // I we have no corresponding functions available at link time,
 // provide weak ones that call kernel through kernel_driver_api
 
@@ -7,6 +9,8 @@
 errno_t __attribute__((weak))
 pod_dev_link( struct pod_driver *drv, struct pod_device *dev )
 {
+	if( (0 == drv) || (0 == drv->kernel_driver_api) || (0 == drv->kernel_driver_api->pod_dev_link) ) return ENOSYS;
+
 	return drv->kernel_driver_api->pod_dev_link( dev, dev );
 }
 
@@ -14,5 +18,7 @@ pod_dev_link( struct pod_driver *drv, struct pod_device *dev )
 errno_t	__attribute__((weak))	
 pod_dev_unlink( struct pod_driver *drv, struct pod_device *dev )
 {
+	if( (0 == drv) || (0 == drv->kernel_driver_api) || (0 == drv->kernel_driver_api->pod_dev_unlink) ) return ENOSYS;
+
 	return drv->kernel_driver_api->pod_dev_unlink( dev, dev );
 }
